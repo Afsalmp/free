fa08b9fe1a25ae34927482b3d4fc9a061306d34a
Afsal <afsalrahmanafsal989@gmail.com>
2023-12-14 19:40:54 +0530

    lw2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ' app',
+      title: 'afsal app',
       theme: ThemeData(
         // This is the theme of your application.
         //
